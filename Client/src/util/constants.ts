2fa7c9c846e82eeeb06189d81144519c5510e397
Growing-Jiwoo <fbaa4412@naver.com>
2023-08-01 19:25:00 +0900

    feat: localStorage, token 기능 구현 및 상수 보관 파일 생성 (#13)

---
+++ Client/src/util/constants.ts
@@ -0,0 +1,8 @@
+export const ACCESS_TOKEN = 'accessToken';
+export const REFRESH_TOKEN = 'refreshToken';
+
+export const PATH_NAME = {
+  SIGN_IN: '/signin',
+  SIGN_UP: '/signup',
+  StockInfo: '/stock',
+};
