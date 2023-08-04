e41db10d631c6f994a7af7111c4adccd3d6a87cd
Growing-Jiwoo <fbaa4412@naver.com>
2023-08-04 11:38:21 +0900

    feat: react-query, react-toastify 셋팅 (#20)

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,30 +1,22 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
-import NavBar from './component/Commons/Navbar';
-import StockInfo from './component/Commons/Main/StockInfo';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div
-      style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}
-    >
-      <div style={{ flex: 1 }}>
-        <Routes>
-          <Route element={<CommonLayout />}>
-            <Route path="/main" element={<StockInfo />} />
-          </Route>
-        </Routes>
-      </div>
-    </div>
-  );
-}
-
-const CommonLayout = () => {
-  return (
     <>
-      <NavBar />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <ToastContainer position="top-center" autoClose={1500} />
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </>
   );
-};
+}
 
 export default App;
