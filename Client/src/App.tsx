8f15e8ee4bb961c0f9fefef096b7bc34db40e80c
Growing-Jiwoo <fbaa4412@naver.com>
2023-08-18 14:58:34 +0900

    feat: queryClient 파일 분리 및 전역 에러 핸들링 구현 (#31)

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,11 +1,10 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-const queryClient = new QueryClient();
+import { queryClient } from './react-query/queryClient';
 
 function App() {
   return (
