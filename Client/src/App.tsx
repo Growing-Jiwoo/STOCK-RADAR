461497485d3169db0ecced62c842d3d4c4fd8d7b
Growing-Jiwoo <fbaa4412@naver.com>
2023-08-22 10:20:56 +0900

    Revert "fix: 공통 컴포넌트 스타일 적용 (#34)"
    This reverts commit 8ee478077d7d5ee70c47697c4917af181ec76f3d.

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -5,16 +5,13 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { queryClient } from './react-query/queryClient';
-import { S } from './style/center.styled';
 
 function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <ToastContainer position="top-center" autoClose={1500} />
-        <S.Center>
-          <RouterProvider router={router} />
-        </S.Center>
+        <RouterProvider router={router} />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </>
