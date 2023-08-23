9598e527444939ab4ffa3d966235e9c45fd9f9e5
Growing-Jiwoo <fbaa4412@naver.com>
2023-08-23 14:38:36 +0900

    fix: 공통 스타일 코드 변경 및 적용 (#38)

--- Client/src/style/center.styled.ts
+++ Client/src/style/center.styled.ts
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 const Center = styled.div`
   display: flex;
   height: 100vh;
-  width: 50%;
+  width: 80%;
   margin: 0 auto;
-  justify-content: center;
+  // justify-content: center;
   align-items: center;
   flex-direction: column;
 `;
