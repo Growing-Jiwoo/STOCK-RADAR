4ba14d04d4980aae4b22bcb53a67be5d624d2183
Growing-Jiwoo <fbaa4412@naver.com>
2023-09-06 20:10:56 +0900

    feat: Token 타입 추가 및 refreshToken 발급 및 스토리지 저장 로직 추가 구현 (#44)

--- Client/src/services/auth.ts
+++ Client/src/services/auth.ts
@@ -1,16 +1,18 @@
 import { useMutation } from '@tanstack/react-query';
-import { Auth } from '../types/auth';
+import { Auth, Token } from '../types/auth';
 import { signin, signup } from '../apis/auth';
 import storage from '../utils/localStorage';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '../utils/constants';
 
 export const useSignIn = () => {
   const navigate = useNavigate();
   return useMutation({
     mutationFn: (req: Auth) => signin(req),
-    onSuccess: (res: { token: string }) => {
-      storage.set('accessToken', res.token);
+    onSuccess: (res: Token) => {
+      storage.set(ACCESS_TOKEN, res.access_token);
+      storage.set(REFRESH_TOKEN, res.refresh_token);
       navigate('/stock');
     },
   });
