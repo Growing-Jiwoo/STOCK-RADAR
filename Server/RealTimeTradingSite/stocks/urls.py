2484bc033ae6528fadf92c7db60770593429a50e
Growing-Jiwoo <fbaa4412@naver.com>
2023-09-25 22:24:48 +0900

    fix: 클래스명 충돌로 인한 클래스명 수정 (#58)

--- Server/RealTimeTradingSite/stocks/urls.py
+++ Server/RealTimeTradingSite/stocks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from .views import StockInfoList, UserSigninAPIView, UserSignupAPIView, RefreshTokenAPIView, UserStocksCreate, \
-    SellStocksAPIView, StockInfoDetail, StockPriceHistory
+    SellStocksAPIView, StockInfoDetail, StockPriceHistoryDays
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('stockinfo', StockInfoList.as_view(), name='stock_info_list'),
     path('stockinfo/<int:pk>', StockInfoDetail.as_view(), name='stock-detail'),
-    path('stock_price_history/<str:stock_id>/<int:days>/', StockPriceHistory.as_view(), name='stock_price_history'),
+    path('stock_price_history/<str:stock_id>/<int:days>/', StockPriceHistoryDays.as_view(), name='stock_price_history'),
     path('purchase_stock', UserStocksCreate.as_view(), name='user-stocks-create'),
     path('sell_stock', SellStocksAPIView.as_view(), name='user-stocks-create'),
     path('signin', UserSigninAPIView.as_view(), name='user-signin'),
