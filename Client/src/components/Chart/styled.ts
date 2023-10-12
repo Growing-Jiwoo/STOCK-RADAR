fa6cbeaca96156578fcb5b9e2b9690dd6c639db4
Growing-Jiwoo <fbaa4412@naver.com>
2023-10-12 21:31:54 +0900

    feat: 주식 가격 히스토리 차트(echart) 구현 (#64)

---
+++ Client/src/components/Chart/styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ChartContainer = styled.div`
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ChartWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
