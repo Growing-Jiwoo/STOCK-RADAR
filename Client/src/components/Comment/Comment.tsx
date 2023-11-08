8a0f113cfebb0791e2e3a7dc7c62309787532a8a
Growing-Jiwoo <fbaa4412@naver.com>
2023-11-08 20:28:51 +0900

    refactor: 댓글 삭제 기능 하드코딩 수정 (#87)

--- Client/src/components/Comment/Comment.tsx
+++ Client/src/components/Comment/Comment.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
-import { usernameState } from '../../recoil/auth/atoms';
-import { modalState } from '../../recoil/board/atoms';
+import { commentIdState, modalState } from '../../recoil/board/atoms';
 import { useCommentList, useEditComment } from '../../services/board';
 import { CommentData, EditComment } from '../../types/board';
 import { StockDetailParams } from '../../types/stock';
@@ -27,7 +26,7 @@ import {
 // to do list
 // 1. 조회한 주식 정보에 해당하는 댓글 갯수만큼 댓글 컴포넌트 생성 (ok)
 // 2. 댓글 생성 시 댓글 생성한 user와 접속한 user가 동일한 댓글에만 수정, 삭제 버튼 보여줌 (ok)
-// 3. 댓글 수정 기능 구현
+// 3. 댓글 수정 기능 구현 (ok)
 // 4. 댓글 생성, 수정, 삭제 시 변경된 서버데이터가 컴포넌트에 바로 반영될 수 있게끔 하기
 // 5. 댓글 수정 버튼 클릭 시 모든 input이 동시에 움직이는 문제 해결 (ok)
 // 6. 댓글 삭제 popup 하드코딩 된 부분 comment_id 들어갈 수 있게끔 하기
@@ -41,6 +40,7 @@ function Comment() {
   const [commentText, setCommentText] = useState<string>('');
   const getCommentList = useCommentList(stockName as string);
   const [, setModal] = useRecoilState(modalState);
+  const [, setCommentId] = useRecoilState(commentIdState);
 
   const handleModifyBtnClick = (index: number, initialText: string) => {
     setIsModifyBtnClicked(index);
@@ -51,9 +51,9 @@ function Comment() {
     setIsModifyBtnClicked(null);
   };
 
-  const editCommentMutation = useEditComment();
-
   const handleModifySuccessBtnClick = (commentId: number) => {
+    const editCommentMutation = useEditComment();
+
     const editCommentText: EditComment = {
       comment_text: commentText,
     };
@@ -62,7 +62,8 @@ function Comment() {
     setIsModifyBtnClicked(null);
   };
 
-  const openPopup = () => {
+  const openPopup = (comment_id: number) => {
+    setCommentId(comment_id);
     setModal({ isOpen: true });
   };
 
@@ -94,7 +95,11 @@ function Comment() {
                     >
                       수정
                     </CommentModifyBtn>
-                    <CommentDeleteBtn onClick={openPopup}>
+                    <CommentDeleteBtn
+                      onClick={() => {
+                        openPopup(comment_id);
+                      }}
+                    >
                       삭제
                     </CommentDeleteBtn>
                   </CommentBtnContainer>
