c03cc2f403aae8dcb22b11954afbc03bdf3652fa
Growing-Jiwoo <fbaa4412@naver.com>
2023-11-09 21:24:56 +0900

    refactor: 차트에 표기되는 시간 데이터를 실시간으로 변경 (#89)

--- Client/src/utils/addMinutesAndFormat.ts
+++ Client/src/utils/addMinutesAndFormat.ts
@@ -1,25 +1,11 @@
-export const addMinutesAndFormat = (
-  dateString: string,
-  minutesToAdd: number
-) => {
-  const dateObject = new Date(dateString);
+export const getCurrentTimeStamp = () => {
+  const currentDate = new Date();
+  currentDate.setHours(currentDate.getHours() + 9);
 
-  dateObject.setMinutes(dateObject.getMinutes() + minutesToAdd);
+  const formattedDate = currentDate
+    .toISOString()
+    .slice(0, 19)
+    .replace('T', ' ');
 
-  const convertTimeStamp = `${dateObject.getFullYear()}-${(
-    dateObject.getMonth() + 1
-  )
-    .toString()
-    .padStart(2, '0')}-${dateObject
-    .getDate()
-    .toString()
-    .padStart(2, '0')} ${dateObject
-    .getHours()
-    .toString()
-    .padStart(2, '0')}:${dateObject
-    .getMinutes()
-    .toString()
-    .padStart(2, '0')}:00`;
-
-  return convertTimeStamp;
+  return formattedDate;
 };
