da8178e590a37b35f659dd0fc5ec7c03a2fdfc17
Growing-Jiwoo <fbaa4412@naver.com>
2023-11-10 20:58:41 +0900

    feat: 댓글 수정 낙관적 업데이트 적용 (#91)

--- Client/src/services/board.ts
+++ Client/src/services/board.ts
@@ -6,38 +6,81 @@ import {
   editComment,
   getCommentList,
 } from '../apis/board';
+import { queryClient } from '../react-query/queryClient';
 import { APIResponse } from '../types/api';
-import { CommentData, CreateComment, EditComment } from '../types/board';
+import { CommentData, CreateComment } from '../types/board';
 import { QUERY_KEYS } from '../utils/constants';
 
 export const useCreateComment = () => {
   return useMutation<CreateComment, AxiosError, CreateComment>(
-    (commentContent: CreateComment) => createComment(commentContent),
-    {
-      mutationKey: [QUERY_KEYS.CREATE_COMMENT],
-    }
+    [QUERY_KEYS.CREATE_COMMENT],
+    (commentContent: CreateComment) => createComment(commentContent)
   );
 };
 
-export const useEditComment = () => {
+export const useEditComment = (stockName: string) => {
   return useMutation<
     APIResponse<unknown>,
     AxiosError,
-    { commentId: number; editCommentText: EditComment }
+    { commentId: number; commentText: string }
   >(
-    ({ commentId, editCommentText }) => editComment(commentId, editCommentText),
+    [QUERY_KEYS.EDIT_COMMENT],
+    ({ commentId, commentText }) =>
+      editComment(commentId, { comment_text: commentText }),
     {
-      mutationKey: [QUERY_KEYS.EDIT_COMMENT],
+      onMutate: async (newTodo) => {
+        // 현재 요청을 취소하고 이전 쿼리 데이터를 백업합니다.
+        await queryClient.cancelQueries({
+          queryKey: [`${QUERY_KEYS.GET_COMMENT_LIST}/${stockName}`],
+        });
+
+        // 이전 댓글 목록 데이터를 가져오기
+        const previousTodos: Array<{
+          comment_id: number;
+          comment_text: string;
+        }> =
+          queryClient.getQueryData([
+            `${QUERY_KEYS.GET_COMMENT_LIST}/${stockName}`,
+          ]) || [];
+
+        // 수정된 댓글이 있으면 해당 데이터를 업데이트
+        const updatedData = previousTodos.map((comment) => {
+          if (comment.comment_id === newTodo.commentId) {
+            return { ...comment, comment_text: newTodo.commentText };
+          }
+          return comment;
+        });
+
+        // 수정된 데이터로 쿼리 데이터를 업데이트.
+        queryClient.setQueryData(
+          [`${QUERY_KEYS.GET_COMMENT_LIST}/${stockName}`],
+          updatedData
+        );
+
+        // 이전 데이터를 반환하여 나중에 롤백할 때 사용.
+        return { previousTodos };
+      },
+      onError: (err, newTodo, context: any) => {
+        // onError시 이전 데이터로 롤백
+        queryClient.setQueryData(
+          [`${QUERY_KEYS.GET_COMMENT_LIST}/${stockName}`],
+          context.previousTodos
+        );
+      },
+      onSettled: () => {
+        // mutation이 완료되면 해당 쿼리를 무효화하여 최신 데이터를 다시 불러옴.
+        queryClient.invalidateQueries([
+          `${QUERY_KEYS.GET_COMMENT_LIST}/${stockName}`,
+        ]);
+      },
     }
   );
 };
 
 export const useDeleteComment = () => {
   return useMutation<APIResponse<unknown>, AxiosError, number>(
-    (commentId: number) => deleteComment(commentId),
-    {
-      mutationKey: [QUERY_KEYS.DELETE_COMMENT],
-    }
+    [QUERY_KEYS.DELETE_COMMENT],
+    (commentId: number) => deleteComment(commentId)
   );
 };
 
@@ -51,7 +94,7 @@ export const useCommentList = (stockName: string) => {
     [`${QUERY_KEYS.GET_COMMENT_LIST}/${stockName}`],
     () => getCommentList(stockName),
     {
-      staleTime: 2 * 60 * 1000,
+      staleTime: 0,
       cacheTime: 10 * 60 * 1000,
     }
   );
