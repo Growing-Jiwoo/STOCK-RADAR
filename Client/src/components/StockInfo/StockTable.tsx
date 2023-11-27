c5db90343efb472d35d20bf51a41411f40c99fc5
Growing-Jiwoo <fbaa4412@naver.com>
2023-11-27 19:49:50 +0900

    refactor: useTableInstance 재사용성을 위한 코드 리팩토링

--- Client/src/components/StockInfo/StockTable.tsx
+++ Client/src/components/StockInfo/StockTable.tsx
@@ -1,19 +1,35 @@
+import { useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Column } from 'react-table';
 import { useSetRecoilState } from 'recoil';
-import { useStockTableInstance } from '../../hooks/useStockTableInstance';
+import { useTableInstance } from '../../hooks/useTableInstance';
 import { selectedStockAtom } from '../../recoil/stockInfo/atoms';
+import { useStockData } from '../../services/stockInfo';
+import { StockInformation } from '../../types/stock';
 import storage from '../../utils/localStorage';
 
 function StockTable() {
-  const tableInstance = useStockTableInstance();
   const navigate = useNavigate();
   const setSelectedStock = useSetRecoilState(selectedStockAtom);
+  const { stockData } = useStockData();
+  const stockTableColumns = useMemo<Column<StockInformation>[]>(
+    () => [
+      { Header: '주식명', accessor: 'name' },
+      { Header: '시가', accessor: 'start_price' },
+      { Header: '현재 가격', accessor: 'current_price' },
+      { Header: '+-%', accessor: 'rate_of_change' },
+      { Header: '어제 종가', accessor: 'yesterday_price' },
+    ],
+    []
+  );
 
   const handleClick = (key: string, value: string) => {
     const MAX_VIEWS = 5;
     const storedData: string | null = storage.get('views');
-    const previousViews = storedData ? JSON.parse(storedData) : {};
-    const newData = { ...previousViews, [key]: value };
+    const previousViews: Record<string, string> = storedData
+      ? JSON.parse(storedData)
+      : {};
+    const newData: Record<string, string> = { ...previousViews, [key]: value };
     const keys = Object.keys(newData);
 
     if (keys.length > MAX_VIEWS) {
@@ -26,6 +42,16 @@ function StockTable() {
 
     setSelectedStock(key);
   };
+
+  const handleRowClick = (rowData: StockInformation) => {
+    const { name, id } = rowData;
+    const stockURL = `/stock/${name}/${id}`;
+
+    navigate(stockURL);
+    handleClick(name, stockURL);
+  };
+
+  const tableInstance = useTableInstance(stockData, stockTableColumns);
 
   return (
     <table className="stock-table" {...tableInstance.getTableProps()}>
@@ -41,22 +67,12 @@ function StockTable() {
       <tbody {...tableInstance.getTableBodyProps()}>
         {tableInstance.rows.map((row) => {
           tableInstance.prepareRow(row);
+          const rowData = tableInstance.data[row.index];
+
           return (
             <tr
-              onClick={() => {
-                navigate(
-                  `/stock/${tableInstance.data[row.index].name}/${
-                    tableInstance.data[row.index].id
-                  }`
-                );
-                handleClick(
-                  `${tableInstance.data[row.index].name}`,
-                  `/stock/${tableInstance.data[row.index].name}/${
-                    tableInstance.data[row.index].id
-                  }`
-                );
-              }}
-              {...row.getRowProps()}
+              onClick={() => handleRowClick(rowData)}
+              {...row.getRowProps({ key: rowData.id })}
             >
               {row.cells.map((cell) => (
                 <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
