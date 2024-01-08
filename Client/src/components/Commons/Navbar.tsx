b113d5757ceeb90bf09ac38e5437d5fbb04065b9
Growing-Jiwoo <fbaa4412@naver.com>
2024-01-08 16:18:30 +0900

    refactor: 주식 가격 react-query 호출 컴포넌트 변경

--- Client/src/components/Commons/Navbar.tsx
+++ Client/src/components/Commons/Navbar.tsx
@@ -2,11 +2,12 @@ import { useNavigate } from 'react-router-dom';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { S } from './styled';
 import useLogout from '../../hooks/useLogout';
+import { useStockData } from '../../services/stockInfo';
 
 function NavBar() {
   const navigate = useNavigate();
   const { logout } = useLogout();
-
+  useStockData();
   return (
     <>
       <S.Header bg="light" variant="light">
@@ -26,6 +27,13 @@ function NavBar() {
             >
               trade
             </Nav.Link> */}
+            <Nav.Link
+              onClick={() => {
+                navigate('/myInfo');
+              }}
+            >
+              myInfo
+            </Nav.Link>
           </Nav>
         </Container>
         <S.LogoutButton onClick={logout}>Logout</S.LogoutButton>
