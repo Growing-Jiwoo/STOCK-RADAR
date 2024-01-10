8249d66c925d12c6b903138d270c13337ca4ea20
Growing-Jiwoo <fbaa4412@naver.com>
2024-01-10 17:56:41 +0900

    perf: reduce를 활용한 성능 개선

--- Client/src/hooks/useStockPossessionDetails.ts
+++ Client/src/hooks/useStockPossessionDetails.ts
@@ -23,45 +23,51 @@ const useStockPossessionDetails = () => {
   const [averageStockPrices, setAverageStockPrices] = useState<number[]>([]);
 
   const [loading, setLoading] = useState(true);
-
   useEffect(() => {
     if (quantityInPossession && stockInPossessionPrice) {
-      const averagePrices = quantityInPossession.map((quantity, idx) => {
-        const averagePrice = (
-          parseFloat(stockInPossessionPrice[idx]) / parseFloat(quantity)
-        ).toFixed(2);
-        return +averagePrice;
-      });
+      const averagePrices = quantityInPossession.reduce(
+        (acc: number[], quantity, idx) => {
+          const averagePrice = (
+            parseFloat(stockInPossessionPrice[idx]) / parseFloat(quantity)
+          ).toFixed(2);
+          acc.push(+averagePrice);
+          return acc;
+        },
+        []
+      );
+
       setAverageStockPrices(averagePrices);
     }
   }, [quantityInPossession, stockInPossessionPrice]);
 
   useEffect(() => {
     if (quantityInPossession) {
-      const price = stockPriceData.map(
-        (arr: { currentPrice: number }, idx: number) => {
-          const returnPrice = Number(
-            (arr.currentPrice * parseFloat(quantityInPossession[idx])).toFixed(
-              2
-            )
-          );
-          return returnPrice;
-        }
-      );
+      const prices = stockPriceData.reduce((acc: number[], arr, idx) => {
+        const returnPrice = Number(
+          (arr.currentPrice * parseFloat(quantityInPossession[idx])).toFixed(2)
+        );
+        acc.push(returnPrice);
+        return acc;
+      }, []);
 
-      setStockReturnPrices(price);
+      setStockReturnPrices(prices);
     }
   }, [stockData]);
 
   useEffect(() => {
     if (stockReturnPricesprices && stockInPossessionPrice) {
-      const differences = stockReturnPricesprices.map((price, idx) => {
-        const stockInPossession = parseFloat(stockInPossessionPrice[idx]);
-        const diff = parseFloat(
-          (((price - stockInPossession) / stockInPossession) * 100).toFixed(2)
-        );
-        return diff;
-      });
+      const differences = stockReturnPricesprices.reduce(
+        (acc: number[], price, idx) => {
+          const stockInPossession = parseFloat(stockInPossessionPrice[idx]);
+          const diff = parseFloat(
+            (((price - stockInPossession) / stockInPossession) * 100).toFixed(2)
+          );
+          acc.push(diff);
+          return acc;
+        },
+        []
+      );
+
       if (differences.length > 0) {
         setReturnPricePercentage(differences);
         setLoading(false);
