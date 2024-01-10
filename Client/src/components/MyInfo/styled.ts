dddffb6e0deda26c5e05fb8db7ce3a425dcd8dc4
Growing-Jiwoo <fbaa4412@naver.com>
2024-01-10 16:45:50 +0900

    refactor: 필요 커스텀 훅 호출을 통한 데이터 로직 수정 및 UI 수정

--- Client/src/components/MyInfo/styled.ts
+++ Client/src/components/MyInfo/styled.ts
@@ -1,19 +1,29 @@
 import styled from 'styled-components';
 
 export const StockListContainer = styled.div`
+  font-family: var(--font-nanumfont);
   border: 1px solid black;
   display: flex;
   justify-content: space-around;
   align-items: center;
-  width: 45vw;
-  min-width: 480px;
+  width: 40vw;
+  min-width: 550px;
   height: 100px;
-  gap: 20px;
 `;
 
-export const StockName = styled.span`
+export const StockPossessionContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-flow: column wrap;
+`;
+
+export const StockPossessionTitle = styled.span`
   width: 200px;
   font-family: var(--font-nanumfontB);
-  font-size: 16px;
+  font-size: 15px;
   text-align: center;
+`;
+
+export const StockPossessionContent = styled.span`
+  font-size: 12px;
 `;
